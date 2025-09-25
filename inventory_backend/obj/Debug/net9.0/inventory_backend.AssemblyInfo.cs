ef5c8b8f546c9c2c658e9135f543f79fb336baba
TRAINEE-SUMAYLO <TRAINEE-SUMAYLO@FGT-08>
2025-09-25 17:58:36 +0800

    Added chakra ui

--- inventory_backend/obj/Debug/net9.0/inventory_backend.AssemblyInfo.cs
+++ inventory_backend/obj/Debug/net9.0/inventory_backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7179b7f5f0fc91d7fee19011c9be29e30a76e264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858bc6019a6eb583f8b7187fd52454befd921133")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ inventory_frontend/src/test.jsx
@@ -0,0 +1,10 @@
+import { Button, HStack } from "@chakra-ui/react";
+
+export const Demo = () => {
+    return(
+        <HStack>
+            <Button>Nigga</Button>
+            <Button>Bitch</Button>
+        </HStack>
+    )
+}
